b3922bca4b950871976668b3d9bc9ff3823cf862
Antra  Gupta <115134630+antra1947@users.noreply.github.com>
2025-03-06 23:25:48 +0530

    Add files via upload

---
+++ 36mergesort.cpp
@@ -0,0 +1,59 @@
+//merge sort - divide problem into subproblem;conquer solve the subproblem;
+//combine sol to set final output
+
+
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int arr[],int l,int mid,int r){
+    int an = mid-l+1;
+    int bn = r-mid;
+    //create 2 temp arr
+    int a[an];
+    int b[bn];
+    for(int i=0;i<an;i++){
+        a[i]=arr[l+i];
+    }
+    for(int j=0;j<bn;j++){
+        b[j]=arr[mid+1+j];
+    }
+    int i=0;//initial index of first subarr,a
+
+    int j=0;//second,b
+    int k=l;//initial index of merged subarr
+    while(i<an && j<bn){
+        if(a[i]<b[j]){
+            arr[k++]=a[i++];
+        }else{
+            arr[k++] = b[j++];
+        }
+    }
+    while(i<an){
+        arr[k++] = a[i++];
+    }
+    while(j<bn){
+        arr[k++]=b[j++];
+    }
+}
+void mergeSort(int arr[],int l,int r){
+    //base case 
+    if(l>=r){
+        return;
+    }
+    int mid = (l+r)/2;
+    mergeSort(arr,l,mid);
+    mergeSort(arr,mid+1,r);
+    merge(arr,l,mid,r);
+}
+
+int main(){
+    int arr[] = {10,20,40,34,56,76,14};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    mergeSort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+cout<<endl;
+
+
+
+}
